d619c9010e145980c226539b5bd89cf692129315
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 21:51:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,4 +33,7 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
+
+  def self.create(name:, grade:)
+  end
 end
