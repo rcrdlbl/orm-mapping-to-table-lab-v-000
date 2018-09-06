350b5b19cb5b3f925aaa7ded97ae991fafcca3c6
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 21:33:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,11 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def self.create_table
   end
 end
