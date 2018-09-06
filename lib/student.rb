fa8dcd7dbac94516437b226b7965c755e5f1313f
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-06 21:38:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,5 +10,13 @@ class Student
   end
 
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
   end
 end
